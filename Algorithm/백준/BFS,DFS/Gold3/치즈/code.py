f61061e77738fa623ffd9dd0a4a15ad826cbde1a
creepereye <creepereye1204@gmail.com>
2025-01-30 20:34:45 +0900

    '2025/01/30'

--- Algorithm/백준/BFS,DFS/Gold3/치즈/code.py
+++ Algorithm/백준/BFS,DFS/Gold3/치즈/code.py
@@ -2,49 +2,65 @@ from collections import deque
 
 n, m = map(int, input().split(" "))
 graph = [list(map(int, input().split(" "))) for _ in range(n)]
-flage = True
-time = -1
+time = 0
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
 
-def bfs(j, i, visited):
+def out(j, i):
+    visited = [[False] * m for _ in range(n)]
     q = deque([[j, i]])
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
+
+    visited[i][j] = True
+    graph[i][j] = -1
+    while q:
+        x, y = q.popleft()
+
+        for k in range(4):
+            x_ = dx[k] + x
+            y_ = dy[k] + y
+            if 0 <= x_ < m and 0 <= y_ < n and not visited[y_][x_] and graph[y_][x_] != 1:
+                visited[y_][x_] = True
+
+                q.append((x_, y_))
+                graph[y_][x_] = -1
+
+
+def bfs(j, i):
+    out(j, i)
+    visited = [[False] * m for _ in range(n)]
+    q = deque([[j, i]])
+
     visited[i][j] = True
     trash = []
     while q:
         x, y = q.popleft()
-        cnt = 0
+
         for k in range(4):
             x_ = dx[k] + x
             y_ = dy[k] + y
-            if 0 <= x_ < m and 0 <= y_ < n and graph[y_][x_] == 0:
-                cnt += 1
-        if cnt > 1:
-            trash += [[x, y]]
-        for k in range(4):
-            x_ = dx[k] + x
-            y_ = dy[k] + y
-            if 0 <= x_ < m and 0 <= y_ < n and graph[y_][x_] == 1 and not visited[y_][x_]:
+            if 0 <= x_ < m and 0 <= y_ < n and not visited[y_][x_]:
                 visited[y_][x_] = True
-                q += [[x_, y_]]
+                if graph[y_][x_] == -1:
+                    q.append((x_, y_))
+                elif graph[y_][x_] == 1:
+                    cnt = 0
+                    for h in range(4):
+                        x__ = dx[h] + x_
+                        y__ = dy[h] + y_
+                        if 0 <= x__ < m and 0 <= y__ < n and graph[y__][x__] == -1:
+                            cnt += 1
+                    if cnt > 1:
+                        trash.append((x_, y_))
 
     for x, y in trash:
-        graph[y][x] = 0
+        graph[y][x] = -1
+    if trash:
+        return True
+    return False
 
 
-while flage:
-    flage = False
+while bfs(0, 0):
     time += 1
-    visited = [[False] * m for _ in range(n)]
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 1 and not visited[i][j]:
-                bfs(j, i, visited)
-                flage = True
-    print()
-    for t in graph:
-
-        print(*t)
 
 print(time, end="")
