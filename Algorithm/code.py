80f371fc957dea5734b9edbc67f1d3766016c94c
creepereye1204 <creepereye1204@gmail.com>
2025-02-27 21:50:12 +0900

    '2025/02/27'

--- Algorithm/code.py
+++ Algorithm/code.py
@@ -1,51 +1,22 @@
-import sys
-
-sys.setrecursionlimit(10**8)
-rst = 0
 n = int(input())
-board = [list(map(int, input().split(" "))) for _ in range(n)]
-dx = [1, 1, 0]
-dy = [0, 1, 1]
+arr = []
+rst = [0] * 1002
+first = True
+pre_index = -1
+for _ in range(n):
+    l, h = map(int, input().split(" "))
+    arr += [[h, l]]
 
-
-def dfs(x, y, state):
-    global board, dx, dy, n, rst
-
-    if x == n - 1 and y == n - 1:
-        rst += 1
-
-        return
-    if state == 0:
-        for i in range(2):
-            x_ = x + dx[i]
-            y_ = y + dy[i]
-            if 0 <= x_ < n and 0 <= y_ < n and board[y_][x_] == 0:
-                if i == 1:
-                    if board[y_ - 1][x_] == 0 and board[y_][x_ - 1] == 0:
-                        dfs(x_, y_, i)
-                else:
-                    dfs(x_, y_, i)
-    elif state == 1:
-        for i in range(3):
-            x_ = x + dx[i]
-            y_ = y + dy[i]
-            if 0 <= x_ < n and 0 <= y_ < n and board[y_][x_] == 0:
-                if i == 1:
-                    if board[y_ - 1][x_] == 0 and board[y_][x_ - 1] == 0:
-                        dfs(x_, y_, i)
-                else:
-                    dfs(x_, y_, i)
+arr.sort(reverse=True)
+for height, index in arr:
+    if first:
+        pre_index = index
+        first = False
+        rst[index] = height
     else:
-        for i in range(1, 3):
-            x_ = x + dx[i]
-            y_ = y + dy[i]
-            if 0 <= x_ < n and 0 <= y_ < n and board[y_][x_] == 0:
-                if i == 1:
-                    if board[y_ - 1][x_] == 0 and board[y_][x_ - 1] == 0:
-                        dfs(x_, y_, i)
-                else:
-                    dfs(x_, y_, i)
+        start = min(pre_index, index)
+        end = max(pre_index, index) + 1
+        for i in range(start, end):
+            rst[i] = max(rst[i], height)
 
-
-dfs(1, 0, 0)
-print(rst)
+print(sum(rst))
