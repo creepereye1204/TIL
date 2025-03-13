12ca930e68fd669ae1ce415b679bab130c48828d
creepereye1204 <creepereye1204@gmail.com>
2025-03-13 17:48:16 +0900

    '2025/03/13'

---
+++ Algorithm/code.js
@@ -0,0 +1,3 @@
+asd;
+
+sconst express = require("express");
--- Algorithm/code.py
+++ Algorithm/code.py
@@ -1,11 +1,36 @@
-import itertools
-import sys
+from collections import deque
 
-# 순열 생성
-perm = itertools.permutations(range(50), 5)
+def bfs(dist,x,y,graph):
+    q=deque([[x,0]])
+    visited=set([x])
+    
+    while q:
+        node,dis=q.popleft()
+        for i in graph[node]:
+            if i not in visited:
+                visited.add(i)
+                q.append([i,dis+dist[node][i]])
+                if i==y:
+                    return q[-1][1]
+                
+def solution(dist,graph,qs):
+    for x,y in qs:
+        print(bfs(dist,x,y,graph))
+    
 
-# 메모리 사용량 측정
-perm_list = list(perm)  # 모든 순열을 리스트로 변환하여 메모리 사용량 측정
-memory_usage = sys.getsizeof(perm_list) / (1024 * 1024)  # MB로 변환
+n,m=map(int,input().split(' '))
+dist=[[-1]*(n+1) for _ in range(n+1)]
+graph=[[] for _ in range(n+1)]
+qs=[]
+for _ in range(n-1):
+    a,b,c=map(int,input().split(' '))
+    dist[a][b]=c
+    dist[b][a]=c
+    graph[a].append(b)
+    graph[b].append(a)
 
-print(f"메모리 사용량: {memory_usage:.2f} MB")
+for _ in range(m):
+    a,b=map(int,input().split(' '))
+    qs.append([a,b])
+
+solution(dist,graph,qs)
