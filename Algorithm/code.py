28876ce32c08d5e9cdf50b1d13940e3eb73278f0
creepereye1204 <creepereye1204@gmail.com>
2025-03-20 20:09:04 +0900

    '2025/03/20'

--- Algorithm/code.py
+++ Algorithm/code.py
@@ -1,7 +1,15 @@
-def solution(k, arr):
-    answers = sorted(list(map(int, arr.split(' '))))
-    return sum(sorted([answers[i]-answers[i-1] for i in range(1, len(answers))], reverse=True)[k-1:] if k-1 < len(answers) else [0])
+t, w = map(int, input().split())
+data = [0] + [int(input()) for _ in range(t)]
+dp = [[0 for _ in range(w+1)] for _ in range(t+1)]
+for i in range(1, t+1):
+    if data[i] == 1:
+        dp[i][0] = dp[i-1][0] + 1
+    else:
+        dp[i][0] = dp[i-1][0]
 
-
-input()
-print(solution(int(input()), input()))
+    for j in range(1, w + 1):
+        if (data[i] == 1 and j % 2 == 0) or (data[i] == 2 and j % 2 != 0):
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-1])
+print(max(dp[t]))
