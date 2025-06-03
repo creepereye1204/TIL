10ab15f860b3070d68e0c52497e366bc6c123328
creepereye1204 <creepereye1204@gmail.com>
2025-06-03 11:13:51 +0900

    2025/06/03

--- Algorithm/프로그래머스/구현/Level1/성격 유형 검사하기/code.py
+++ Algorithm/프로그래머스/구현/Level1/성격 유형 검사하기/code.py
@@ -1,15 +1,17 @@
-def solution(surveies, choices):
-    answer = ''
+def solution(survey, choices):
+    scores = {"RT": 0, "CF": 0, "JM": 0, "AN": 0}
+    for s, c in zip(survey, choices):
+        score = c - 4
+        if score != 0:
+            key = "".join(sorted(s))
+            scores[key] += score * (1 if s[0] in "RCJA" else -1)
 
-    dic = {'RT': {'T': 0, 'R': 0}, 'CF': {'C': 0, 'F': 0},
-           'JM': {'J': 0, 'M': 0}, 'AN': {'A': 0, 'N': 0}}
-    for survey, choice in zip(surveies, choices):
-        key = ''.join(sorted(list(survey)))
-        dic[key][survey[0]] += max(4-choice, 0)
-        dic[key][survey[1]] += max(choice-4, 0)
-    for key in dic.keys():
-        arr = []
-        for k, v in dic[key].items():
-            arr.append((-v, k))
-        answer = answer+sorted(arr)[0][1]
-    return answer
+    result = ""
+    for types in ["RT", "CF", "JM", "AN"]:
+        if scores[types] > 0:
+            result += types[1]
+        elif scores[types] < 0:
+            result += types[0]
+        else:
+            result += types[0]
+    return result
